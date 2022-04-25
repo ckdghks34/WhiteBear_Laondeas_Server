92290572c03da0c317510217517ee8996304c6e4
Heo ChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-04-26 02:25:19 +0900

    ################ # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성 # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 # 예) feat : 로그인 기능 추가 feat : 광고신청 API 기능 추가 # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    - 광고 신청 등록, 광고 신청 목록 가져오기 기능 API 추가
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################

---
+++ routes/controller/authController.js
@@ -0,0 +1,165 @@
+import mysql from "mysql";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import dbconfig from "../../config/dbconfig.js";
+
+const conn = mysql.createConnection(dbconfig);
+
+// 회원가입
+async function signup(req, res, next) {
+  console.log(signup);
+
+  let {
+    user_id,
+    user_password,
+    user_name,
+    user_nickname,
+    user_email,
+    user_phonenumber,
+    agreement_info,
+    agreement_email,
+    agreement_mms,
+    is_advertiser,
+    is_admin,
+  } = req.body;
+
+  let now = new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
+  user_password = bcrypt.hashSync(user_password, 10);
+  const sql = `Insert into user (id, password, name, nickname, email, phonenumber, agreement_info, agreement_email, agreement_mms, is_advertiser,first_register_date,last_register_date,is_admin) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)`;
+  conn.query(
+    sql,
+    [
+      user_id,
+      user_password,
+      user_name,
+      user_nickname,
+      user_email,
+      user_phonenumber,
+      agreement_info,
+      agreement_email,
+      agreement_mms,
+      is_advertiser,
+      now,
+      now,
+      is_admin,
+    ],
+    (err, result) => {
+      if (err) console.log(err);
+
+      if (result) {
+        res.status(200).json({
+          message: "회원가입 성공",
+        });
+      } else {
+        res.status(401).json({
+          message: "회원가입 실패",
+        });
+      }
+    }
+  );
+}
+
+// 로그인
+async function login(req, res, next) {
+  const { user_id, user_password } = req.body;
+
+  if (user_id && user_password) {
+    console.log(user_id, user_password);
+    const sql = `SELECT * FROM user WHERE id = ?`;
+    conn.query(sql, user_id, (err, results) => {
+      // 에러 발생 시  error 출력
+      if (err) {
+        console.log(err);
+      }
+      // 아이디가 존재하지 않을 때
+      if (results.length === 0) {
+        res.status(401).json({
+          message: "아이디가 존재하지 않습니다.",
+        });
+        console.log("아이디가 존재하지 않습니다.");
+      } else {
+        // 입력받은 비밀번호와 데이터베이스에 저장된 비밀번호(암호화 된) 비교
+        bcrypt.compare(user_password, results[0].password, (err, result) => {
+          if (err) {
+            console.log(err);
+          }
+
+          // 비밀번호가 맞으면
+          if (result) {
+            let loginUser = results[0];
+            loginUser.password = undefined;
+
+            res.status(200).json({
+              message: "로그인 성공",
+              user: loginUser,
+            });
+          }
+          // 비밀번호가 틀리면
+          else {
+            res.status(401).json({
+              message: "아이디 또는 비밀번호가 일치하지 않습니다.",
+            });
+          }
+        });
+      }
+    });
+  }
+}
+
+// 아이디 찾기
+
+// 비밀번호 찾기
+
+// 로그아웃
+
+// 회원탈퇴
+async function deleteUser(req, res, next) {
+  const { user_id } = req.body;
+
+  if (user_id) {
+    const sql = `DELETE FROM user WHERE id = ?`;
+    conn.query(sql, user_id, (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+
+      if (result) {
+        res.status(200).json({
+          message: "회원탈퇴 성공",
+        });
+      } else {
+        res.status(401).json({
+          message: "회원탈퇴 실패",
+        });
+      }
+    });
+  }
+}
+
+// 아이디체크
+async function checkID(req, res, next) {
+  const { user_id } = req.query;
+
+  if (user_id) {
+    const sql = `SELECT * FROM user WHERE id = ?`;
+    conn.query(sql, user_id, (err, results) => {
+      if (err) {
+        console.log(err);
+      }
+
+      if (results.length === 0) {
+        res.status(200).json({
+          message: "사용 가능한 아이디 입니다.",
+          flag: true,
+        });
+      } else {
+        res.status(401).json({
+          message: "이미 아이디가 존재합니다.",
+          flag: false,
+        });
+      }
+    });
+  }
+}
+
+export { signup, login, deleteUser, checkID };
