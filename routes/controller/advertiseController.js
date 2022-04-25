b8da8edca2eba719783bde94157c4c507728b732
Heo ChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-04-26 02:31:06 +0900

    ################ # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성 # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 # 예) feat : 로그인 기능 추가 feat : 광고 기능 컨트롤러 추가 # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    - 광고 신청 목록 가져오기, 광고 신청 등록 기능을 구현한 controller 추가
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################

---
+++ routes/controller/advertiseController.js
@@ -0,0 +1,61 @@
+import mysql from "mysql";
+import dbconfig from "../../config/dbconfig.js";
+
+const conn = mysql.createConnection(dbconfig);
+
+// 광고 신청 등록
+async function createAdvertise(req, res, next) {
+  let { company_name, contact, area, isagency, agreement_info } = req.body;
+
+  // isagecny의 값이 없다면 0으로 설정
+  // 임시
+  if (isagency === undefined) isagency = 0;
+
+  if (company_name && contact && area && agreement_info) {
+    const sql =
+      "Insert into advertisement (company_name, contact, area, isagency, agreement_info, first_register_date, last_register_date) values (?, ?, ?, ?, ?, ?, ?)";
+
+    conn.query(
+      sql,
+      [company_name, contact, area, isagency, agreement_info, new Date(), new Date()],
+      (err, result) => {
+        if (err) console.log(err);
+
+        if (result) {
+          res.status(200).json({
+            message: "광고 신청 성공",
+          });
+        } else {
+          res.status(401).json({
+            message: "광고 신청 실패",
+          });
+        }
+      }
+    );
+  } else {
+    res.status(401).json({
+      message: "광고 신청 실패, 필수 데이터가 누락되었습니다.",
+    });
+  }
+}
+
+// 광고 신청 목록 가져오기
+async function getAdvertiseList(req, res, next) {
+  const sql = "Select * from advertisement";
+  conn.query(sql, (err, result) => {
+    if (err) console.log(err);
+
+    if (result) {
+      res.status(200).json({
+        message: "광고 신청 목록 성공",
+        data: result,
+      });
+    } else {
+      res.status(401).json({
+        message: "광고 신청 목록 실패",
+      });
+    }
+  });
+}
+
+export { createAdvertise, getAdvertiseList };
