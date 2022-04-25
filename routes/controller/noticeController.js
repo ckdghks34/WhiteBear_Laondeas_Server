b91a9b2429578823249c110805fc47d8ad0a905e
Heo ChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-04-26 02:27:27 +0900

    ################ # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성 # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 # 예) feat : 로그인 기능 추가 feat : 공지사항 관련 기능 API 추가 # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    - 공지사항 목록 가져오기, 등록, 수정, 삭제, 특정 공지사항 가져오기, 조회수 증가 API 추가함.
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################

---
+++ routes/controller/noticeController.js
@@ -0,0 +1,159 @@
+import mysql from "mysql";
+import dbconfig from "../../config/dbconfig.js";
+
+const conn = mysql.createConnection(dbconfig);
+
+// 공지사항 가져오기
+async function getNotice(req, res, next) {
+  const sql = "select * from notice";
+  conn.query(sql, (err, results) => {
+    if (err) console.log(err);
+
+    res.status(200).json({
+      message: "공지사항 불러오기 성공",
+      notice: results,
+    });
+  });
+}
+// 공지사항 등록
+async function createNotice(req, res, next) {
+  let { user_seq, title, content } = req.body;
+
+  if (user_seq && title && content) {
+    const sql =
+      "insert into notice (author ,title, content,view_count, first_register_id, first_register_date, last_register_id, last_register_date) values (?, ?, ?, ?, ?, ?, ?, ?)";
+
+    conn.query(
+      sql,
+      [user_seq, title, content, 0, user_seq, new Date(), user_seq, new Date()],
+      (err, result) => {
+        if (err) console.log(err);
+
+        if (result) {
+          res.status(200).json({
+            message: "공지사항 등록 성공",
+          });
+        } else {
+          res.status(401).json({
+            message: "공지사항 등록 실패",
+          });
+        }
+      }
+    );
+  } else {
+    res.status(401).json({
+      message: "공지사항 등록 실패, 필수 데이터가 없습니다.",
+    });
+  }
+}
+// 공지사항 수정
+async function updateNotice(req, res, next) {
+  let { notice_seq, user_seq, title, content } = req.body;
+
+  if (notice_seq && user_seq && title && content) {
+    const sql =
+      "update notice set title = ?, content = ?, last_register_id = ?, last_register_date = ? where notice_seq = ?";
+
+    conn.query(sql, [title, content, user_seq, new Date(), notice_seq], (err, result) => {
+      if (err) console.log(err);
+
+      if (result) {
+        res.status(200).json({
+          message: "공지사항 수정 성공",
+        });
+      } else {
+        res.status(401).json({
+          message: "공지사항 수정 실패",
+        });
+      }
+    });
+  } else {
+    res.status(401).json({
+      message: "공지사항 수정 실패, 필수 데이터가 없습니다.",
+    });
+  }
+}
+
+// 공지사항 삭제
+async function deleteNotice(req, res, next) {
+  let { notice_seq } = req.body;
+
+  if (notice_seq) {
+    const sql = "delete from notice where notice_seq = ?";
+
+    conn.query(sql, [notice_seq], (err, result) => {
+      if (err) console.log(err);
+
+      if (result) {
+        res.status(200).json({
+          message: "공지사항 삭제 성공",
+        });
+      } else {
+        res.status(401).json({
+          message: "공지사항 삭제 실패",
+        });
+      }
+    });
+  } else {
+    res.status(401).json({
+      message: "공지사항 삭제 실패, 필수 데이터가 없습니다.",
+    });
+  }
+}
+
+// 조회수 증가
+async function increaseViewCount(req, res, next) {
+  let { notice_seq } = req.body;
+
+  if (notice_seq) {
+    const sql = "update notice set view_count = view_count + 1 where notice_seq = ?";
+
+    conn.query(sql, [notice_seq], (err, result) => {
+      if (err) console.log(err);
+
+      if (result) {
+        res.status(200).json({
+          message: "조회수 증가 성공",
+        });
+      } else {
+        res.status(401).json({
+          message: "조회수 증가 실패",
+        });
+      }
+    });
+  } else {
+    res.status(401).json({
+      message: "조회수 증가 실패, 필수 데이터가 없습니다.",
+    });
+  }
+}
+
+// 공지사항 상세 조회
+async function getNoticeDetail(req, res, next) {
+  let { notice_seq } = req.query;
+
+  if (notice_seq) {
+    const sql = "select * from notice where notice_seq = ?";
+
+    conn.query(sql, [notice_seq], (err, result) => {
+      if (err) console.log(err);
+
+      if (result) {
+        res.status(200).json({
+          message: "공지사항 상세 조회 성공",
+          notice: result,
+        });
+      } else {
+        res.status(401).json({
+          message: "공지사항 상세 조회 실패",
+        });
+      }
+    });
+  } else {
+    res.status(401).json({
+      message: "공지사항 상세 조회 실패, 공지사항 시퀀스가 없습니다.",
+    });
+  }
+}
+
+export { getNotice, createNotice, updateNotice, deleteNotice, increaseViewCount, getNoticeDetail };
---
+++ routes/notice.js
@@ -0,0 +1,24 @@
+import express from "express";
+import * as noticeController from "./controller/noticeController.js";
+
+const router = express.Router();
+
+// 공지사항 가져오기
+router.get("/", noticeController.getNotice);
+
+// 공지사항 등록
+router.post("/", noticeController.createNotice);
+
+// 공지사항 수정
+router.patch("/", noticeController.updateNotice);
+
+// 공지사항 삭제
+router.delete("/", noticeController.deleteNotice);
+
+// 공지사항 조회수 증가
+router.patch("/view-count", noticeController.increaseViewCount);
+
+// 공지사항 상세 조회
+router.get("/detail", noticeController.getNoticeDetail);
+
+export default router;
