bfc4eb6f2b92ed0c96843d6191e1437c94c3dd4f
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-04-27 17:12:51 +0900

    ################ # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성 # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 # 예) feat : 로그인 기능 추가 feat : 모듈에 따른 함수 수정 # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함)
    ################
    # 본문(구체적인 내용)을 아랫줄에 작성
    # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    - mysql2 async/await를 활용하여 기존에 작성한 함수 수정하였음.
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- routes/controller/faqController.js
+++ routes/controller/faqController.js
@@ -2,71 +2,98 @@ import pool from "../../config/dbpool.js";
 
 // faq 불러오기
 async function getFaq(req, res, next) {
-  const sql = "select * from faq";
-  pool.query(sql, (err, results) => {
-    if (err) console.log(err);
+  try {
+    const sql = "select * from faq";
+
+    const results = await pool.query(sql);
+    console.log(results[0]);
 
     res.status(200).json({
       message: "faq 불러오기 성공",
-      faq: results,
+      faq: results[0],
     });
-  });
+  } catch (err) {
+    console.log(err);
+
+    res.status(400).json({
+      message: "faq 불러오기 실패",
+    });
+  }
 }
 
 // faq 등록
 async function createFaq(req, res, next) {
   const { user_seq, category, title, content } = req.body;
 
-  if (category && title && content) {
-    const sql = `insert into faq (category, title, content, first_register_id, first_register_date, last_register_id, last_register_date) values (?, ?, ?,?,?,?,?)`;
-
-    pool.query(
-      sql,
-      [category, title, content, user_seq, new Date(), user_seq, new Date()],
-      (err, result) => {
-        if (err) console.log(err);
-
-        if (result) {
-          res.status(200).json({
-            message: "faq 등록 성공",
-          });
-        } else {
-          res.status(401).json({
-            message: "faq 등록 실패",
-          });
-        }
-      }
-    );
-  } else {
+  if (
+    user_seq === undefined ||
+    category === undefined ||
+    title === undefined ||
+    content === undefined
+  ) {
     res.status(401).json({
       message: "faq 등록 실패, 필수 항목이 없습니다.",
     });
+  } else {
+    try {
+      const sql = `insert into faq (category, title, content, first_register_id, first_register_date, last_register_id, last_register_date) values (?, ?, ?,?,?,?,?)`;
+
+      const results = await pool.execute(sql, [
+        category,
+        title,
+        content,
+        user_seq,
+        new Date(),
+        user_seq,
+        new Date(),
+      ]);
+
+      res.status(200).json({
+        message: "faq 등록 성공",
+      });
+    } catch (err) {
+      console.log(err);
+
+      res.status(400).json({
+        message: "faq 등록 실패",
+      });
+    }
   }
 }
+
 // faq 수정
 async function updateFaq(req, res, next) {
   const { faq_seq, user_seq, category, title, content } = req.body;
 
-  if (category && title && content) {
-    const sql = `update faq set category = ?, title = ?, content = ?, last_register_id = ?, last_register_date = ? where faq_seq = ?`;
-
-    pool.query(sql, [category, title, content, user_seq, new Date(), faq_seq], (err, result) => {
-      if (err) console.log(err);
-
-      if (result) {
-        res.status(200).json({
-          message: "faq 수정 성공",
-        });
-      } else {
-        res.status(401).json({
-          message: "faq 수정 실패",
-        });
-      }
-    });
-  } else {
-    res.status(401).json({
+  if (
+    faq_seq === undefined ||
+    user_seq === undefined ||
+    category === undefined ||
+    title === undefined ||
+    content === undefined
+  ) {
+    res.status(400).json({
       message: "faq 수정 실패, 필수 항목이 없습니다.",
     });
+  } else {
+    try {
+      const sql = `update faq set category = ?, title = ?, content = ?, last_register_id = ?, last_register_date = ? where faq_seq = ?`;
+
+      const results = await pool.execute(sql, [
+        category,
+        title,
+        content,
+        user_seq,
+        new Date(),
+        faq_seq,
+      ]);
+    } catch (err) {
+      console.log(err);
+
+      res.status(400).json({
+        message: "faq 수정 실패",
+      });
+    }
   }
 }
 
@@ -74,26 +101,26 @@ async function updateFaq(req, res, next) {
 async function deleteFaq(req, res, next) {
   const { faq_seq } = req.body;
 
-  if (faq_seq) {
-    const sql = `delete from faq where faq_seq = ?`;
-
-    pool.query(sql, [faq_seq], (err, result) => {
-      if (err) console.log(err);
-
-      if (result) {
-        res.status(200).json({
-          message: "faq 삭제 성공",
-        });
-      } else {
-        res.status(401).json({
-          message: "faq 삭제 실패",
-        });
-      }
+  if (faq_seq === undefined) {
+    res.status(400).json({
+      message: "faq 삭제 실패, 필수 항목이 없습니다.",
     });
   } else {
-    res.status(401).json({
-      message: "faq 삭제 실패, 해당 faq가 없습니다.",
-    });
+    try {
+      const sql = `delete from faq where faq_seq = ?`;
+
+      const results = await pool.execute(sql, [faq_seq]);
+
+      res.status(200).json({
+        message: "faq 삭제 성공",
+      });
+    } catch (err) {
+      console.log(err);
+
+      res.status(400).json({
+        message: "faq 삭제 실패",
+      });
+    }
   }
 }
 
--- routes/controller/noticeController.js
+++ routes/controller/noticeController.js
@@ -2,72 +2,83 @@ import pool from "../../config/dbpool.js";
 
 // 공지사항 가져오기
 async function getNotice(req, res, next) {
-  const sql = "select * from notice";
-  pool.query(sql, (err, results) => {
-    if (err) console.log(err);
-
+  try {
+    const sql = "select * from notice order by notice_seq desc";
+    const result = await pool.query(sql);
     res.status(200).json({
-      message: "공지사항 불러오기 성공",
-      notice: results,
+      message: "공지사항 가져오기 성공",
+      data: result[0],
     });
-  });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: "공지사항 가져오기 실패",
+    });
+  }
 }
 // 공지사항 등록
 async function createNotice(req, res, next) {
   let { user_seq, title, content } = req.body;
 
-  if (user_seq && title && content) {
-    const sql =
-      "insert into notice (author ,title, content,view_count, first_register_id, first_register_date, last_register_id, last_register_date) values (?, ?, ?, ?, ?, ?, ?, ?)";
-
-    pool.query(
-      sql,
-      [user_seq, title, content, 0, user_seq, new Date(), user_seq, new Date()],
-      (err, result) => {
-        if (err) console.log(err);
-
-        if (result) {
-          res.status(200).json({
-            message: "공지사항 등록 성공",
-          });
-        } else {
-          res.status(401).json({
-            message: "공지사항 등록 실패",
-          });
-        }
-      }
-    );
-  } else {
-    res.status(401).json({
+  if (user_seq === undefined || title === undefined || content === undefined) {
+    res.status(400).json({
       message: "공지사항 등록 실패, 필수 데이터가 없습니다.",
     });
+  } else {
+    try {
+      const sql =
+        "insert into notice (author ,title, content,view_count, first_register_id, first_register_date, last_register_id, last_register_date) values (?, ?, ?, ?, ?, ?, ?, ?)";
+      const result = await pool.execute(sql, [
+        user_seq,
+        title,
+        content,
+        0,
+        user_seq,
+        new Date(),
+        user_seq,
+        new Date(),
+      ]);
+
+      res.status(200).json({
+        message: "공지사항 등록 성공",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({
+        message: "공지사항 등록 실패",
+      });
+    }
   }
 }
 // 공지사항 수정
 async function updateNotice(req, res, next) {
   let { notice_seq, user_seq, title, content } = req.body;
 
-  if (notice_seq && user_seq && title && content) {
-    const sql =
-      "update notice set title = ?, content = ?, last_register_id = ?, last_register_date = ? where notice_seq = ?";
-
-    pool.query(sql, [title, content, user_seq, new Date(), notice_seq], (err, result) => {
-      if (err) console.log(err);
-
-      if (result) {
-        res.status(200).json({
-          message: "공지사항 수정 성공",
-        });
-      } else {
-        res.status(401).json({
-          message: "공지사항 수정 실패",
-        });
-      }
-    });
-  } else {
-    res.status(401).json({
+  if (
+    notice_seq === undefined ||
+    user_seq === undefined ||
+    title === undefined ||
+    content === undefined
+  ) {
+    res.status(400).json({
       message: "공지사항 수정 실패, 필수 데이터가 없습니다.",
     });
+  } else {
+    try {
+      const sql =
+        "update notice set title = ?, content = ?, last_register_id = ?, last_register_date = ? where notice_seq = ?";
+
+      await pool.execute(sql, [title, content, user_seq, new Date(), notice_seq]);
+
+      res.status(200).json({
+        message: "공지사항 수정 성공",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({
+        message: "공지사항 수정 실패",
+      });
+    }
   }
 }
 
@@ -75,26 +86,25 @@ async function updateNotice(req, res, next) {
 async function deleteNotice(req, res, next) {
   let { notice_seq } = req.body;
 
-  if (notice_seq) {
-    const sql = "delete from notice where notice_seq = ?";
-
-    pool.query(sql, [notice_seq], (err, result) => {
-      if (err) console.log(err);
-
-      if (result) {
-        res.status(200).json({
-          message: "공지사항 삭제 성공",
-        });
-      } else {
-        res.status(401).json({
-          message: "공지사항 삭제 실패",
-        });
-      }
-    });
-  } else {
-    res.status(401).json({
+  if (notice_seq === undefined) {
+    res.status(400).json({
       message: "공지사항 삭제 실패, 필수 데이터가 없습니다.",
     });
+  } else {
+    try {
+      const sql = "delete from notice where notice_seq = ?";
+
+      await pool.execute(sql, [notice_seq]);
+
+      res.status(200).json({
+        message: "공지사항 삭제 성공",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({
+        message: "공지사항 삭제 실패",
+      });
+    }
   }
 }
 
@@ -102,26 +112,25 @@ async function deleteNotice(req, res, next) {
 async function increaseViewCount(req, res, next) {
   let { notice_seq } = req.body;
 
-  if (notice_seq) {
-    const sql = "update notice set view_count = view_count + 1 where notice_seq = ?";
-
-    pool.query(sql, [notice_seq], (err, result) => {
-      if (err) console.log(err);
-
-      if (result) {
-        res.status(200).json({
-          message: "조회수 증가 성공",
-        });
-      } else {
-        res.status(401).json({
-          message: "조회수 증가 실패",
-        });
-      }
-    });
-  } else {
-    res.status(401).json({
+  if (notice_seq === undefined) {
+    res.status(400).json({
       message: "조회수 증가 실패, 필수 데이터가 없습니다.",
     });
+  } else {
+    try {
+      const sql = "update notice set view_count = view_count + 1 where notice_seq = ?";
+
+      await pool.execute(sql, [notice_seq]);
+
+      res.status(200).json({
+        message: "조회수 증가 성공",
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({
+        message: "조회수 증가 실패",
+      });
+    }
   }
 }
 
@@ -129,27 +138,25 @@ async function increaseViewCount(req, res, next) {
 async function getNoticeDetail(req, res, next) {
   let { notice_seq } = req.query;
 
-  if (notice_seq) {
-    const sql = "select * from notice where notice_seq = ?";
-
-    pool.query(sql, [notice_seq], (err, result) => {
-      if (err) console.log(err);
-
-      if (result) {
-        res.status(200).json({
-          message: "공지사항 상세 조회 성공",
-          notice: result,
-        });
-      } else {
-        res.status(401).json({
-          message: "공지사항 상세 조회 실패",
-        });
-      }
+  if (notice_seq === undefined) {
+    res.status(400).json({
+      message: "공지사항 상세 조회 실패, 필수 데이터가 없습니다.",
     });
   } else {
-    res.status(401).json({
-      message: "공지사항 상세 조회 실패, 공지사항 시퀀스가 없습니다.",
-    });
+    try {
+      const sql = "select * from notice where notice_seq = ?";
+      const result = await pool.query(sql, [notice_seq]);
+
+      res.status(200).json({
+        message: "공지사항 상세 조회 성공",
+        data: result[0],
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({
+        message: "공지사항 상세 조회 실패",
+      });
+    }
   }
 }
 
