dbdab1b2c0659d334db952d523ac731326f228f6
Heo ChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-04-27 14:39:11 +0900

    ################ # <타입> : <제목> 의 형식으로 제목을 아래 공백줄에 작성 # 제목은 50자 이내 / 변경사항이 "무엇"인지 명확히 작성 / 끝에 마침표 금지 # 예) feat : 로그인 기능 추가 feat: 주소록 관련 APi 추가 # 바로 아래 공백은 지우지 마세요 (제목과 본문의 분리를 위함) - 주소록 가져오기, 등록, 삭제, 수정 기능 추가 ################ # 본문(구체적인 내용)을 아랫줄에 작성 # 여러 줄의 메시지를 작성할 땐 "-"로 구분 (한 줄은 72자 이내)
    ################
    # 꼬릿말(footer)을 아랫줄에 작성 (현재 커밋과 관련된 이슈 번호 추가 등)
    # 예) Close #7
    
    ################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # test : 테스트 코드 추가
    # refact : 코드 리팩토링
    # style : 코드 의미에 영향을 주지 않는 변경사항
    # chore : 빌드 부분 혹은 패키지 매니저 수정사항
    ################

--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,39 @@ const router = express.Router();
 
 // 특정 아이디 조회하기
 router.get("/", userController.getUser);
+
+// 회원 정보 수정
+router.patch("/", userController.updateUser);
+
+/**
+ * 유저 주소록
+ */
+
+// 주소록 가져오기
+router.get("/address", userController.getAddressBook);
+
+// 주소록 등록
+router.post("/address", userController.createAddressBook);
+
+// 주소록 삭제
+router.delete("/address", userController.deleteAddressBook);
+
+// 주소록 수정
+router.patch("/address", userController.updateAddressBook);
+
+/**
+ * 유저 페널티
+ */
+
+// 페널티 조회
+router.get("/penalty", userController.getPenaltyList);
+
+// 페널티 등록
+router.post("/penalty", userController.createPenalty);
+
+// 페널티 수정
+router.patch("/penalty", userController.updatePenalty);
+
+// 페널티 삭제
+router.delete("/penalty", userController.deletePenalty);
 export default router;
