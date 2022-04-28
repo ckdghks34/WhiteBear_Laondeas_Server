c2235e7c08edf743db50c97727cb1eda54183e6e
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-04-28 20:32:34 +0900

    feat : JWT 토큰 적용
    - JWT 토큰 적용 활성화

--- routes/faq.js
+++ routes/faq.js
@@ -1,18 +1,19 @@
 import express from "express";
+import { authJWT } from "../middleware/jwt/authJWT.js";
 import * as faqController from "./controller/faqController.js";
 
 const router = express.Router();
 
 // faq 가져오기
-router.get("/", faqController.getFaq);
+router.get("/", authJWT, faqController.getFaq);
 
 // faq 등록
-router.post("/", faqController.createFaq);
+router.post("/", authJWT, faqController.createFaq);
 
 // faq 수정
-router.patch("/", faqController.updateFaq);
+router.patch("/", authJWT, faqController.updateFaq);
 
 // faq 삭제
-router.delete("/", faqController.deleteFaq);
+router.delete("/", authJWT, faqController.deleteFaq);
 
 export default router;
