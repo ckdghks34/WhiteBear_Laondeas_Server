19952040df3928a0cf5e87a64b665eafaf4a8a38
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-04-29 05:52:34 +0900

    refact : DB connection pool 반환
    - 트랜잭션 사용을 위해 conncetion pool 생성 후 반환

--- config/dbpool.js
+++ config/dbpool.js
@@ -3,4 +3,4 @@ import dbconfig from "./dbconfig.js";
 
 const pool = mysql.createPool(dbconfig);
 
-export default pool.promise();
+export default pool.promise().getConnection(async (conn) => conn);
