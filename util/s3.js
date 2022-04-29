724d00bd3034d4051b8420db8360398ac6e8329a
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-04-30 03:28:54 +0900

    feat : 파일업로드 기능 추가
    - 파일 업로드를 위해 설정 파일 수정

--- util/s3.js
+++ util/s3.js
@@ -1,9 +1,13 @@
 import AWS from "aws-sdk";
-import multer from "multer";
-import multerS3 from "multer-s3";
+import dotenv from "dotenv";
+dotenv.config();
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: "ap-northeast-2",
 });
+
+const bucket = process.env.BUCKET;
+
+export { s3, bucket };
