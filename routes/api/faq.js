ebbdb5a4f6c993ba462e9fd1dba2f905aedddbe5
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-04-30 09:43:10 +0900

    - 테스트를 위해 임시 JWT 인증 해제

--- routes/api/faq.js
+++ routes/api/faq.js
@@ -5,15 +5,15 @@ import * as faqController from "./controller/faqController.js";
 const router = express.Router();
 
 // faq 가져오기
-router.get("/", authJWT, faqController.getFaq);
+router.get("/", /* authJWT, */ faqController.getFaq);
 
 // faq 등록
-router.post("/", authJWT, faqController.createFaq);
+router.post("/", /* authJWT, */ faqController.createFaq);
 
 // faq 수정
-router.patch("/", authJWT, faqController.updateFaq);
+router.patch("/", /* authJWT, */ faqController.updateFaq);
 
 // faq 삭제
-router.delete("/", authJWT, faqController.deleteFaq);
+router.delete("/", /* authJWT, */ faqController.deleteFaq);
 
 export default router;
