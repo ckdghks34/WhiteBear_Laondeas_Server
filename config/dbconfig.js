f52fc17c3311dc2021e524cf61c19e547d267a1c
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-05-04 21:35:39 +0900

    fix : 데이터베이스 Datetime 컬럼 반환값 수정

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -3,12 +3,14 @@ dotenv.config();
 
 const config = {
   host: process.env.database_host,
+  port: process.env.database_port,
   user: process.env.database_user,
   password: process.env.database_password,
   database: process.env.database_database,
   connectionLimit: 2,
   // keepAliveInitialDay: 10000,
   enableKeepAlive: true,
+  dateStrings: "datetime",
 };
 
 export default config;
