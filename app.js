bde6f2b2d1b0e225675a7288dae4c05c4157dd67
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-05-13 11:23:46 +0900

    https server on

--- app.js
+++ app.js
@@ -66,14 +66,14 @@ app.listen(process.env.SERVER_PORT, () => {
   console.log("server is running...");
 });
 
-// https
-// const options = {
-//   key: fs.readFileSync("/etc/letsencrypt/live/laonlaonlaon.ml/privkey.pem"),
-//   cert: fs.readFileSync("/etc/letsencrypt/live/laonlaonlaon.ml/fullchain.pem"),
-//   ca: fs.readFileSync("/etc/letsencrypt/live/laonlaonlaon.ml/chain.pem"),
-// };
-// var httpsserver = https.createServer(options, app);
+https;
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/laonlaonlaon.ml/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/laonlaonlaon.ml/fullchain.pem"),
+  ca: fs.readFileSync("/etc/letsencrypt/live/laonlaonlaon.ml/chain.pem"),
+};
+var httpsserver = https.createServer(options, app);
 
-// httpsserver.listen(process.env.HTTPS_SERVER_PORT, () => {
-//   console.log(`https server(${process.env.HTTPS_SERVER_PORT}) is running...`);
-// });
+httpsserver.listen(process.env.HTTPS_SERVER_PORT, () => {
+  console.log(`https server(${process.env.HTTPS_SERVER_PORT}) is running...`);
+});
