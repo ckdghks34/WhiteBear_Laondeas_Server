60d30c73d26a78042d4312205bb43afa3b0f563f
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-05-18 18:09:37 +0900

    통계 다운로드 API 추가 구현

--- routes/api/statistics.js
+++ routes/api/statistics.js
@@ -7,4 +7,9 @@ const router = express.Router();
 // 통게
 router.get("/", /*authJWT,*/ statisticsController.getStatistics);
 
+// download Statistics
+router.get("/download", /*authJWT,*/ statisticsController.downloadStatistics);
+// Test xlsx
+router.get("/test", statisticsController.getTest);
+
 export default router;
