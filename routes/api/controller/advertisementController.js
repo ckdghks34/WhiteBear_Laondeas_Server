4ba1e5f7d36c84fd22f3117473b4f29f6d43de80
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-05-20 23:38:58 +0900

    - 광고 가져오기 반환값 is_pending 추가 - 광고 신청 승인, 반려 기능 추가

--- routes/api/controller/advertisementController.js
+++ routes/api/controller/advertisementController.js
@@ -19,7 +19,7 @@ async function createAdvertise(req, res, next) {
   } else {
     try {
       const sql =
-        "Insert into advertisement (company_name, contact, area, isagency, agreement_info, first_register_date, last_register_date) values (?, ?, ?, ?, ?, ?, ?)";
+        "Insert into advertisement (company_name, contact, area, is_pending, isagency, agreement_info, first_register_date, last_register_date) values (?, ?, ?, 1, ?, ?, ?, ?)";
 
       const results = await dbpool.execute(sql, [
         company_name,
@@ -35,7 +35,7 @@ async function createAdvertise(req, res, next) {
         message: "광고 신청 등록 성공",
       });
     } catch (err) {
-      console.log(err);
+      console.error(err);
 
       res.status(500).json({
         message: "광고 신청 등록 실패",
@@ -55,7 +55,7 @@ async function getAdvertiseList(req, res, next) {
       advertise: results[0],
     });
   } catch (err) {
-    console.log(err);
+    console.error(err);
 
     res.status(500).json({
       message: "광고 신청 목록 가져오기 실패",
@@ -63,4 +63,57 @@ async function getAdvertiseList(req, res, next) {
   }
 }
 
-export { createAdvertise, getAdvertiseList };
+// 광고 신청 승인
+async function approveAdvertise(req, res, next) {
+  const { advertising_seq } = req.body;
+
+  if (advertising_seq === undefined) {
+    res.status(400).json({
+      message: "광고 신청 승인 실패, 필수 항목이 없습니다.",
+    });
+  } else {
+    try {
+      const sql = "update advertisement set is_pending = 0 where advertising_seq = ?";
+
+      const results = await dbpool.execute(sql, [advertising_seq]);
+
+      res.status(200).json({
+        message: "광고 신청 승인 성공",
+      });
+    } catch (err) {
+      console.error(err);
+
+      res.status(500).json({
+        message: "광고 신청 승인 실패",
+      });
+    }
+  }
+}
+
+// 광고 신청 반려
+async function rejectAdvertise(req, res, next) {
+  const { advertising_seq } = req.body;
+
+  if (advertising_seq === undefined) {
+    res.status(400).json({
+      message: "광고 신청 반려 실패, 필수 항목이 없습니다.",
+    });
+  } else {
+    try {
+      const sql = "update advertisement set is_pending = -1 where advertising_seq = ?";
+
+      const results = await dbpool.execute(sql, [advertising_seq]);
+
+      res.status(200).json({
+        message: "광고 신청 반려 성공",
+      });
+    } catch (err) {
+      console.error(err);
+
+      res.status(500).json({
+        message: "광고 신청 반려 실패",
+      });
+    }
+  }
+}
+export { createAdvertise, getAdvertiseList, approveAdvertise, rejectAdvertise };
