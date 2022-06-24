773f1e2b2721b3f99d7df1642ce0fe3b04e3b3ff
HeoChangHwan <45061747+ckdghks34@users.noreply.github.com>
2022-06-24 14:36:19 +0900

    nice 인증 테스트

--- routes/api/nice.js
+++ routes/api/nice.js
@@ -4,9 +4,17 @@ import pgInfo from "./../../config/pginfo.js";
 const router = express.Router();
 
 // 암호화 데이터(키) 발급
-router.all("/encrypt/data", niceController.createSecretKey);
+// router.all("/encrypt/data", niceController.createSecretKey);
+
+router.get("/encrypt/data", niceController.createSecretKey);
+
+router.post("/encrypt/data", niceController.createSecretKey);
 
 // callback & redirect
-router.all("/decrypt/data", niceController.decryptData);
+// router.all("/decrypt/data", niceController.decryptData);
+
+router.get("/decrypt/data", niceController.decryptData);
+
+router.post("/decrypt/data", niceController.decryptData);
 
 export default router;
